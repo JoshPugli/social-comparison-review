d5750cace1fb27db0e8f2b237846750cdd6dae73
Josh Puglielli <jdpuglielli@gmail.com>
2024-02-12 13:40:55 -0500

    works

--- frontend/src/pages/Platform/Platform.js
+++ frontend/src/pages/Platform/Platform.js
@@ -1,21 +1,52 @@
 import React, { useState } from "react";
 import styles from "./Platform.module.scss";
 import { Icon } from "@iconify/react";
-import {MdiInstagram} from "../../assets/icons/icons";
+import { MdiInstagram } from "../../assets/icons/icons";
 
 const Platform = () => {
   const [selectedPlatform, setSelectedPlatform] = useState(null);
   const platforms = [
-    { id: 1, name: "Facebook", icon: <Icon icon="ic:baseline-facebook" />, colorClass: "facebookColor" },
-    { id: 2, name: "Twitter", icon: <Icon icon="mdi:twitter" />, colorClass: "twitterColor" },
-    { id: 3, name: "Instagram", icon: <Icon icon="mdi:instagram" />, alternativeIcon: <MdiInstagram />, colorClass: "instagramColor" },
-    { id: 4, name: "Reddit", icon: <Icon icon="ic:baseline-reddit" />, colorClass: "redditColor" },
-    { id: 5, name: "Snapchat", icon: <Icon icon="mingcute:snapchat-fill" />, colorClass: "snapchatColor" },
-    { id: 6, name: "TikTok", icon: <Icon icon="ic:baseline-tiktok" />, colorClass: "tiktokColor" },
+    {
+      id: 1,
+      name: "Facebook",
+      icon: <Icon icon="ic:baseline-facebook" />,
+      colorClass: "facebookColor",
+    },
+    {
+      id: 2,
+      name: "Twitter",
+      icon: <Icon icon="mdi:twitter" />,
+      colorClass: "twitterColor",
+    },
+    {
+      id: 3,
+      name: "Instagram",
+      icon: <Icon icon="mdi:instagram" />,
+      alternativeIcon: <MdiInstagram />,
+      colorClass: "instagramColor",
+    },
+    {
+      id: 4,
+      name: "Reddit",
+      icon: <Icon icon="ic:baseline-reddit" />,
+      colorClass: "redditColor",
+    },
+    {
+      id: 5,
+      name: "Snapchat",
+      icon: <Icon icon="mingcute:snapchat-fill" />,
+      colorClass: "snapchatColor",
+    },
+    {
+      id: 6,
+      name: "TikTok",
+      icon: <Icon icon="ic:baseline-tiktok" />,
+      colorClass: "tiktokColor",
+    },
   ];
 
   const handleClick = (platform) => {
-    setSelectedPlatform(platform.id); 
+    setSelectedPlatform(platform.id);
   };
 
   return (
@@ -30,7 +61,11 @@ const Platform = () => {
             onClick={() => handleClick(platform)}
             className={`${styles.platform} ${
               selectedPlatform === platform.id ? styles.selected : ""
-            } ${selectedPlatform === platform.id ? styles[platform.colorClass] : ""}`}
+            } ${
+              selectedPlatform === platform.id
+                ? styles[platform.colorClass]
+                : ""
+            }`}
           >
             {/* Render both icons for Instagram but control visibility */}
             {platform.id === 3 ? (
@@ -59,7 +94,30 @@ const Platform = () => {
             ) : (
               <span className={styles.icon}>{platform.icon}</span>
             )}
-            <span className={styles.platformText}>{platform.name}</span>
+            {platform.id === 3 ? (
+              <div className={styles.platformTextWrapper}>
+                <span
+                  className={styles.platformText}
+                  style={{
+                    opacity: selectedPlatform === platform.id ? 0 : 1,
+                    transition: "opacity 0.3s ease-out",
+                  }}
+                >
+                  {platform.name} 
+                </span>
+                <span
+                  className={`${styles.platformText} ${styles.instagramGradientText}`}
+                  style={{
+                    opacity: selectedPlatform === platform.id ? 1 : 0,
+                    transition: "opacity 0.3s ease-out",
+                  }}
+                >
+                  {platform.name} 
+                </span>
+              </div>
+            ) : (
+              <span className={styles.platformText}>{platform.name}</span>
+            )}
           </div>
         ))}
       </div>
@@ -68,4 +126,3 @@ const Platform = () => {
 };
 
 export default Platform;
-
