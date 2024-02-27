8bdacf269bb9f638ded639dacb78cccf19526c84
Josh Puglielli <jdpuglielli@gmail.com>
2024-02-27 15:39:25 -0500

    added new favicon

--- backend/machine-learn/ml.py
+++ backend/machine-learn/ml.py
@@ -25,34 +25,6 @@ from sklearn.metrics import f1_score, accuracy_score
 if os.environ.get("ENV") != "production":
     load_dotenv()
 
-DISTORTIONS = [
-    {
-        "Filtering": {
-            "Definitions": [
-                "Mental filtering is draining and straining all positives in a situation and, instead, dwelling on its negatives.",
-                "Even if there are more positive aspects than negative in a situation or person, you focus on the negatives exclusively.",
-            ],
-            "Examples": [
-                "It’s performance review time at your company, and your manager compliments your hard work several times. In the end, they make one improvement suggestion. You leave the meeting feeling miserable and dwell on that one suggestion all day long."
-            ],
-        }
-    },
-    {"All-or-nothing thinking": {"Definitions": [], "Examples": []}},
-    {"Overgeneralization": {"Definitions": [], "Examples": []}},
-    {"Discounting the positive": {"Definitions": [], "Examples": []}},
-    {"Jumping to conclusions": {"Definitions": [], "Examples": []}},
-    {"Catastrophizing": {"Definitions": [], "Examples": []}},
-    {"Personalization": {"Definitions": [], "Examples": []}},
-    {"Control fallacies": {"Definitions": [], "Examples": []}},
-    {"Fallacy of fairness": {"Definitions": [], "Examples": []}},
-    {"Blaming": {"Definitions": [], "Examples": []}},
-    {"Shoulds": {"Definitions": [], "Examples": []}},
-    {"Emotional reasoning": {"Definitions": [], "Examples": []}},
-    {"Fallacy of change": {"Definitions": [], "Examples": []}},
-    {"Global labeling": {"Definitions": [], "Examples": []}},
-    {"Always being right": {"Definitions": [], "Examples": []}},
-]
-
 
 def generate_distortions(
     platform, usage, thought, thought_score, emotion, emotion_score, situation
