433fc14dd66ee96499d0cdd5a7241eadfb898618
Paul Bangalan <160093920+pauljban@users.noreply.github.com>
2024-03-31 12:05:32 -0400

    Improve mobile spacing for Distortion, Reframe, and End pages

--- frontend/src/pages/End/End.js
+++ frontend/src/pages/End/End.js
@@ -2,8 +2,14 @@ import React from "react";
 import styles from "./End.module.scss"; // Ensure the correct path is used for the CSS module
 import Lottie from "react-lottie";
 import animationData from "../../assets/animations/Animation - 1711487741365.json";
+import { useScreenWidth } from "../../utils/screenWidth";
 
 const End = () => {
+
+  const screenWidth = useScreenWidth();
+  const size = screenWidth < 700 ? 250 : 400;
+
+
   const defaultOptions = {
     loop: false,
     autoplay: true,
@@ -16,7 +22,7 @@ const End = () => {
   return (
     <div className={styles.container}>
         <div className={styles.content}>
-      <Lottie options={defaultOptions} height={400} width={400} />
+      <Lottie options={defaultOptions} height={size} width={size} />
       <div className={styles.header}>Thank you for completing the survey</div>
       </div>
     </div>
