ce165943778aaa534f45ad637003355c62776947
Josh Puglielli <jdpuglielli@gmail.com>
2024-04-08 19:23:25 -0400

    kinda done

--- frontend/src/pages/End/End.js
+++ frontend/src/pages/End/End.js
@@ -2,78 +2,68 @@ import React from "react";
 import styles from "./End.module.scss"; // Ensure the correct path is used for the CSS module
 import Lottie from "react-lottie";
 import animationData from "../../assets/animations/Animation - 1711487741365.json";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Icon } from "@iconify/react";
 
-const End = () => {
-  const location = useLocation();
-  // const { selections } = location.state || {};
-  // TODO Remove hardcoded selections
+const End = ({selections}) => {
+  const handleRestart = () => {
+    sessionStorage.clear(); // Clear the session storage
+    window.location.reload();
+  };
 
-  // const [emotionData, setEmotionData] = useState(selections[currentPage] || {
-  //   beliefRating: "",
-  //   emotion: "",
-  //   emotionIntensity: "",
-  // });
-  const selections = [
-    "I'm not good enough",
-    {
-      beliefRating: "10",
-      emotion: "sad",
-      emotionIntensity: "10",
-    },
-    "I failed a test I studied hard for",
-    ["All-or-Nothing Thinking"],
-    "It is not the end of the world. I can learn from this experience and do better next time.",
-    "It is not the end of the world. I can learn from this experience and do better next time.",
-  ];
-  console.log("Selections:", selections);
-  const defaultOptions = {
-    loop: false,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
+  const handleDownload = () => {
+    const dataStr = JSON.stringify(selections, null, 2);
+    const blob = new Blob([dataStr], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "ThoughtRecord.json";
+    link.click();
+    URL.revokeObjectURL(url);
   };
 
   return (
     <div className={styles.outer}>
-    <div className={styles.container}>
-      {/* <Lottie options={defaultOptions} height={200} width={200} /> */}
-      <div className={styles.header}>
-        Let's look back at what you came up with:
-      </div>
-      <div className={styles.content}>
-        <div className={styles.selections}>
-          <div className={styles.selection}>
-            <span className={styles.colored}>Negative thought I had:</span>{" "}
-            {selections[0]}
-          </div>
-          <div className={styles.selection}>
-            <span className={styles.colored}>Emotion I felt:</span>{" "}
-            {selections[1].emotion}
-          </div>
-          <div className={styles.selection}>
-            <span className={styles.colored}>Situation I was in:</span>{" "}
-            {selections[2]}
-          </div>
-          <div className={styles.selection}>
-            <span className={styles.colored}>
-              Thinking trap(s) I identified:
-            </span>{" "}
-            {selections[3].join(", ")}
-          </div>
-          <div className={styles.selection}>
-            <span className={styles.colored}>Reframe I came up with:</span>{" "}
-            {selections[5]}
+      <div className={styles.container}>
+        {/* <Lottie options={defaultOptions} height={200} width={200} /> */}
+        <div className={styles.header}>
+          Let's look back at what you came up with:
+        </div>
+        <div className={styles.content}>
+          <div className={styles.selections}>
+            <div className={styles.selection}>
+              <span className={styles.colored}>Negative thought I had:</span>{" "}
+              {selections[0]}
+            </div>
+            <div className={styles.selection}>
+              <span className={styles.colored}>Emotion I felt:</span>{" "}
+              {selections[1].emotion}
+            </div>
+            <div className={styles.selection}>
+              <span className={styles.colored}>Situation I was in:</span>{" "}
+              {selections[2]}
+            </div>
+            <div className={styles.selection}>
+              <span className={styles.colored}>
+                Thinking trap(s) I identified:
+              </span>{" "}
+              {selections[3].join(", ")}
+            </div>
+            <div className={styles.selection}>
+              <span className={styles.colored}>Reframe I came up with:</span>{" "}
+              {selections[5]}
+            </div>
           </div>
         </div>
+        <button className={styles.downloadButton} onClick={handleDownload}>
+          Download Thought Record
+        </button>
       </div>
-      <button className={styles.downloadButton}>Download Thought Record</button>
-    </div>
 
-    <button className={styles.tryAgain}><Icon style={{marginRight: "5px"}} icon="iconamoon:restart-bold" /> Try this activity again</button>
+      <button className={styles.tryAgain} onClick={handleRestart}>
+        <Icon style={{ marginRight: "5px" }} icon="iconamoon:restart-bold" />{" "}
+        Try this activity again
+      </button>
     </div>
   );
 };
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -9,15 +9,38 @@ import Situation from "../Situation/Situation";
 import Emotion from "../Emotion/Emotion";
 import Finalize from "../Finalize/Finalize";
 import { stages } from "../../assets/constants";
-import { useNavigate } from "react-router-dom";
 import End from "../End/End";
 
 const Home = () => {
-  const [currentPage, setCurrentPage] = useState(5); // Change this to 0 to start from the beginning
-  const [selections, setSelections] = useState(Array(stages.length).fill(null));
-  const [isFinal, setIsFinal] = useState(false);
-  const navigate = useNavigate();
-  const components = [Thought, Emotion, Situation, Distortion, Reframe, Finalize, End];
+  const [currentPage, setCurrentPage] = useState(() => {
+    const savedPage = sessionStorage.getItem("currentPage");
+    return savedPage ? JSON.parse(savedPage) : 0; // Change this default value as needed
+  });
+  const [selections, setSelections] = useState(() => {
+    const savedSelections = sessionStorage.getItem("selections");
+    return savedSelections
+      ? JSON.parse(savedSelections)
+      : Array(stages.length).fill(null);
+  });
+  const [isFinal, setIsFinal] = useState(() => {
+    const savedIsFinal = sessionStorage.getItem("isFinal");
+    return savedIsFinal ? JSON.parse(savedIsFinal) : false;
+  });
+
+  useEffect(() => {
+    sessionStorage.setItem("currentPage", JSON.stringify(currentPage));
+    sessionStorage.setItem("selections", JSON.stringify(selections));
+    sessionStorage.setItem("isFinal", JSON.stringify(isFinal));
+  }, [currentPage, selections, isFinal]);
+  const components = [
+    Thought,
+    Emotion,
+    Situation,
+    Distortion,
+    Reframe,
+    Finalize,
+    End,
+  ];
   const componentProps = {
     Thought: {
       selections,
@@ -79,7 +102,9 @@ const Home = () => {
   };
 
   const CurrentComponent = isFinal ? End : components[currentPage];
-  const props = isFinal ? componentProps['End'] : componentProps[stages[currentPage]];
+  const props = isFinal
+    ? componentProps["End"]
+    : componentProps[stages[currentPage]];
 
   const handleBack = () => {
     setCurrentPage((prev) => Math.max(prev - 1, 0));
@@ -96,17 +121,19 @@ const Home = () => {
 
   return (
     <div className="page-container">
-      <HeaderBar progress={currentPage} sections={stages} isFinal={isFinal}/>
+      <HeaderBar progress={currentPage} sections={stages} isFinal={isFinal} />
       <div className="home-container">
         {React.createElement(CurrentComponent, props)}
       </div>
-      {!isFinal &&<Footer
-        onBack={handleBack}
-        onForward={handleForward}
-        canContinue={canContinue()}
-        canGoBack={currentPage > 0}
-        currentPage={currentPage}
-      /> }
+      {!isFinal && (
+        <Footer
+          onBack={handleBack}
+          onForward={handleForward}
+          canContinue={canContinue()}
+          canGoBack={currentPage > 0}
+          currentPage={currentPage}
+        />
+      )}
     </div>
   );
 };
